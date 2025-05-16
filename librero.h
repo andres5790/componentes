af4744fecd566752b8a033d9c15336114278fb6b
andres5790 <andresjarrinramirez@gmail.com>
2025-05-16 17:12:19 -0500

    Update librero.h

--- librero.h
+++ librero.h
@@ -1,4 +1,9 @@
-void producto(char productos[30], int demanda[5], int tiempo[5], int recursos[5], int recursos2[5],int recursos3[5],int recursos4[5]);
+int producto(char productos[][30], int demanda[5], int tiempo[5], int *recursos, int *recursos2, int *recursos3, int *recursos4);
 int tiempot(int demanda[5], int tiempo[5], int op);
-void caso(int tiempot, int tiempo[5],int op,int t);
-int recu(int *recurso, int *recursos2, int *recursos3t, int *recursos4t, int op, int demanda[5]);
+void caso(int tiempot,int t,int s);
+void recu(int op, int demanda[5],int *recurso, int *recursos2, int *recursos3t, int *recursos4t );
+int menu(int opc);
+int resta(int *recurso, int *recursos2, int *recursos3t, int *recursos4t,int *r1,int *r2,int *r3,int *r4);
+void restat(int tiempot,  int *t);
+void comprobar(int *recurso, int *recursos2, int *recursos3t, int *recursos4t,int *r1,int *r2,int *r3,int *r4,int *n);
+void cambio(char productos[][30], int demanda[], int tiempo[]);
