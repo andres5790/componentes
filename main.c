794811f5fe4e44c5340311419ef4387a21e02761
andres5790 <andresjarrinramirez@gmail.com>
2025-05-21 22:22:47 -0500

    Update main.c

--- main.c
+++ main.c
@@ -41,12 +41,12 @@ int main(int argc, char *argv[])
                         switch (opc)
                         {
                         case 1:
-                            tiempocliente(tiempoc,t,&s);
+                            tiempocliente(&t,&s);
                             comprobar(&recursos, &recursos2, &recursos3, &recursos4, &tr1, &tr2, &tr3, &tr4, &n);
                             printf("Valor de s: %d, Valor de n: %d\n", s, n);
                             if (s == 1 && n == 4)
                             {
-                                printf("si se puede realizar el pedido ya que existe suficiente demanda\n");
+                                printf("si se puede realizar el pedido ya que existe suficiente demanda y se puede cumplir en el tiempo establecido\n");
                                 resta(&recursos, &recursos2, &recursos3, &recursos4, &tr1, &tr2, &tr3, &tr4);
                                 
                             }
@@ -75,7 +75,6 @@ int main(int argc, char *argv[])
                             printf("el stock de pantallas actuales es de %d\n", recursos2);
                             printf("el stock de microfonos actuales es de %d\n", recursos3);
                             printf("el stock de altavoces actuales es de %d\n", recursos4);
-                            printf("el tiempo restante es de %d\n", tiempos);
                             break;
                         case 5:
                             agregar(op, cantidad, &recursos, &recursos2, &recursos3, &recursos4);
